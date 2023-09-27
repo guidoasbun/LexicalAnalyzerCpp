ceeb49b4dee6ba1c4c493ba68eca28b0e467bd36
guidoasbun <guido@asbun.io>
2023-09-27 12:53:01 -0700

    Add project files.

---
+++ LexicalAnalyzerCpp/LexicalAnalyzerCpp.cpp
@@ -0,0 +1,33 @@
+﻿// LexicalAnalyzerCpp.cpp : Defines the entry point for the application.
+//
+
+#include "LexicalAnalyzerCpp.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	fstream testFile;
+	testFile.open("test.txt", ios::in);
+	if(testFile.is_open())
+	{
+		string line;
+		while (getline(testFile,line))
+		{
+			if (line.empty()) continue;
+
+
+			cout << line << endl;
+		}
+		testFile.close();
+	}
+	else
+	{
+		cout << "Error opening file";
+	}
+
+
+	return 0;
+}
