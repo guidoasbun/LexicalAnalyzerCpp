3b987808c93aee35669256c5424511c8696dfeee
guidoasbun <guido@asbun.io>
2023-09-27 17:04:10 -0700

    function seperation initialized

--- LexicalAnalyzerCpp/LexicalAnalyzerCpp.cpp
+++ LexicalAnalyzerCpp/LexicalAnalyzerCpp.cpp
@@ -4,29 +4,68 @@
 #include "LexicalAnalyzerCpp.h"
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
-int main()
+const string CAP_LETTERS = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const string DIGITS = "0123456789";
+const string LETTERS_DIGITS = "_ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+const string SINGLE_OPERATORS = "{}[]#()?.+-*/%^&|~!=<>";
+const string DOUBLE_OPERATORS = "## <: ";
+
+// Function prototypes
+void parse(const string& fileName);
+vector<string> parseIdentifier(const string& input);
+void printTokens(const vector<string>& tokens);
+
+// Function definitions
+void parse(const string& fileName)
 {
 	fstream testFile;
-	testFile.open("test.txt", ios::in);
+	testFile.open(fileName, ios::in);
+
 	if(testFile.is_open())
 	{
 		string line;
-		while (getline(testFile,line))
+		while(getline(testFile, line))
 		{
-			if (line.empty()) continue;
+			if(line.empty()) continue;
 
-
-			cout << line << endl;
+			parseIdentifier(line);
 		}
 		testFile.close();
 	}
 	else
 	{
-		cout << "Error opening file";
+		cout << "Unable to open file" << endl;
 	}
+
+}
+
+vector<string> parseIdentifier(const string& input)
+{
+	vector <string> tokens;
+
+	
+
+	return tokens;
+}
+
+void printTokens(const vector<string>& tokens)
+{
+		for(auto &token : tokens)
+		{
+			cout << token << endl;
+	}
+}
+
+
+
+int main()
+{
+	const string fileName("test.txt");
+	parse(fileName);
 
 
 	return 0;
